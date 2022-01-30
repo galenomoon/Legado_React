85b096d7e0bf7712f6c7ed250db8a129946d52c3
galeno <gui.galenocover@gmail.com>
2022-01-30 17:46:28 -0300

    Counter | Using Class Components + State

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,32 +1,34 @@
 import React from 'react' //Sintaxe JSX React
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './components/PrimeiroComponente'
-import A, { CompB as B } from './components/DoisComponente'
-import MultiElements from './components/MultiElements'
-import FamiliaSilva from './components/FamiliaSilva'
-import Familia from './components/Familia'
-import Member from './components/Member'
-import ComponentWithFunction from './components/componentWithFunction'
-import Pai from './components/Pai'
-import ComponenteClasse from './components/ClassComponent'
+// import PrimeiroComponente from './components/PrimeiroComponente'
+// import A, { CompB as B } from './components/DoisComponente'
+// import MultiElements from './components/MultiElements'
+// import FamiliaSilva from './components/FamiliaSilva'
+// import Familia from './components/Familia'
+// import Member from './components/Member'
+// import ComponentWithFunction from './components/componentWithFunction'
+// import Pai from './components/Pai'
+// import ComponenteClasse from './components/ClassComponent'
+import Contador from './components/Contador'
 
 
 //single page application | get div #root of index.html
 const element = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <ComponenteClasse value="I'm a class component" />
+        <Contador numero={0} />
+            {/* <ComponenteClasse value="I'm a class component" /> */ }
         {/* <Pai/> */}
-        {/* <ComponentWithFunction/> */}
-        {/* <MultiElements/> */}
-        {/* <PrimeiroComponente value={'Good Morning'}/> */}
-        {/* <A value={'Olá, eu sou A'} />
+            {/* <ComponentWithFunction/> */}
+            {/* <MultiElements/> */}
+            {/* <PrimeiroComponente value={'Good Morning'}/> */}
+            {/* <A value={'Olá, eu sou A'} />
         <B value={'Tudo bem, sou B?'} /> */}
-        {/* <FamiliaSilva sobrenome=" Silva">
+            {/* <FamiliaSilva sobrenome=" Silva">
     <Member nome="Guilherme" />
     <Member nome="Luana" sobrenome="Galeno"/>
 </FamiliaSilva> */}
-        {/* <Familia sobrenome=" Pereira ">
+            {/* <Familia sobrenome=" Pereira ">
     <Member nome="Ednaldo" />
     <Member nome="Apollo" />
 </Familia> */}
