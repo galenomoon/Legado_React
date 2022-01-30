2f4dad473dc145c8b529a6393d52228ca45e88de
galeno <gui.galenocover@gmail.com>
2022-01-30 15:53:34 -0300

    Start Point

---
+++ src/components/PrimeiroComponente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function primeiro(){
+
+    return <h1>Primeiro Componente</h1>
+
+}
+
+export default primeiro
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react' //Sintaxe JSX React
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//single page application | get div #root of index.html
+const element = document.getElementById('root')
+const jsx = <h1>Olá React</h1>
+ReactDOM.render(jsx, element)
