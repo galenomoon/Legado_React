8c6615921ca863abd5cdb8a994710a7ca047f004
galeno <gui.galenocover@gmail.com>
2022-01-30 16:48:28 -0300

    relationship between components

---
+++ fundamentos-react/src/components/FamiliaSilva.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Member from "./Member";
+
+export default props=>
+<div>
+    <Member nome='Lucas' sobrenome='Silva'/>
+    <Member nome='Ana' sobrenome='Silva'/>
+    <Member nome='Maria' sobrenome='Silva'/>
+    <Member nome='João' sobrenome='Silva'/>
+</div>
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
 import A, { CompB as B } from './components/DoisComponente'
 import MultiElements from './components/MultiElements'
+import FamiliaSilva from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Member from './components/Member'
+
 
 //single page application | get div #root of index.html
 const element = document.getElementById('root')
@@ -10,7 +14,12 @@ ReactDOM.render(
     <div>
         {/* <MultiElements/> */}
         {/* <PrimeiroComponente value={'Good Morning'}/> */}
-        <A value={'Olá, eu sou A'} />
-        <B value={'Tudo bem, sou B?'} />
+        {/* <A value={'Olá, eu sou A'} />
+        <B value={'Tudo bem, sou B?'} /> */}
+<Familia>
+    <Member nome="Guilherme" sobrenome="Galeno"/>
+    <Member nome="Luana" sobrenome="Galeno"/>
+</Familia>
+        <FamiliaSilva/>
     </div>
     , element)
