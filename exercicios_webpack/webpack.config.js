fb829a743abe34f561814d43df643868054fe5a0
galeno <gui.galenocover@gmail.com>
2022-01-31 10:47:21 -0300

    Using ECMAScript2015 with Babel

--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -2,12 +2,22 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './ex/index.js',
-    output:{
+    output: {
         path: __dirname + 'public',
         filename: './bundle.js'
     },
-    devServer:{
-        port:8080,
+    devServer: {
+        port: 8080,
         contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015']
+            }
+        }]
     }
 }
