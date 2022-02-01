a1c68d6987ee4bf9c942c44ee775f8e44baff47c
galeno <gui.galenocover@gmail.com>
2022-02-01 15:47:06 -0300

    Counter Finished Project

--- react-react/ex/counter.jsx
+++ react-react/ex/counter.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { inc, dec, stepChanged } from './counterAction'
 
 const Counter = props => (
-    <div>OK</div>
+    <div>
+        <h1>{props.counter.number}</h1>
+        <input onChange={props.stepChanged}
+            value={props.counter.step} type={"number"} />
+        <button onClick={props.inc}>Inc</button>
+        <button onClick={props.dec}>Dec</button>
+    </div>
 )
-export default Counter
+
+const mapStateToProps = (state) => ({ counter: state.counter })
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+    inc, dec, stepChanged
+}, dispatch)
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
